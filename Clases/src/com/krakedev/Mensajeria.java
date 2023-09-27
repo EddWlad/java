bd9acbc8cbaef687a810c500862ccaf716f72953
EddWlad <tidsec_inf@outlook.com>
2023-09-27 15:24:56 -0500

    funcion multiplicar

---
+++ Clases/src/com/krakedev/Mensajeria.java
@@ -0,0 +1,15 @@
+package com.krakedev;
+
+public class Mensajeria 
+{
+	public void saludar()
+	{
+		System.out.println("HOLA HUMANOS");
+	}
+	
+	public void saludarAmigos(String nombre,String apellido)
+	{
+		System.out.println("HOLA "+nombre+" "+apellido);
+	}
+	
+}
